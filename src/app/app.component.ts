76649d04f01c4169e0f0c5f58e1720b09b23331f
technoexponent02 <technoexponen02@gmail.com>
2019-12-23 19:42:16 +0530

    View commits in table (done)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,37 @@ import { ApiService } from './services/api.service';
 })
 export class AppComponent implements OnInit{
   title = 'Crossroads-test';
+  commitData:any = [];
 
   constructor(
     private apiService: ApiService
   ){}
 
   ngOnInit(){
-    this.apiService.getCommits('/Crossroads-test/commits/master').subscribe((res:any) => {
-      console.log(res);
-    })
-    this.apiService.getCommits('/repos').subscribe((res:any) => {
-      console.log(res);
+    this.apiService.getCommits('/Crossroads-test/commits').subscribe((res:any) => {         //get commit data
+      res.forEach(async(ele:any) => {
+        console.log(ele);
+        let dt = await this.convert(ele.commit.committer.date);                             //convert date
+        this.commitData.push({
+          author: ele.commit.committer.name,
+          dt: dt,
+          message: ele.commit.message,
+        })
+      });
+      console.log(this.commitData);
     })
   }
+
+
+convert(timestamp) {
+  console.log(timestamp);
+  var date = new Date(timestamp);               // Convert to date
+
+  console.log(date);
+  return [
+    ("0" + date.getDate()).slice(-2),           // Get day and pad it with zeroes
+    ("0" + (date.getMonth()+1)).slice(-2),      // Get month and pad it with zeroes
+    date.getFullYear()                          // Get full year
+  ].join('/');                                  // Glue the pieces together
+}
 }
